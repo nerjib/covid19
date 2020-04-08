cee48c6dd1d4d11620b48ac786b191c16f0a5f1d
nerjib <kabirnajib0@gmail.com>
2020-04-08 19:29:19 +0000

    computation of impact and severeimpact

---
+++ _test_/estimator.spec.js
@@ -0,0 +1,41 @@
+
+import covid19ImpactEstimator from '../src/estimator'
+
+
+describe("Challenge 1", ()=>{
+    test("checking values of output", ()=>{
+        const input = {
+            region: {
+            name: "Africa",
+            avgAge: 19.7,
+            avgDailyIncomeInUSD: 5,
+            avgDailyIncomePopulation: 0.71
+            },
+            periodType: "days",
+            timeToElapse: 58,
+            reportedCases: 674,
+            population: 66622705,
+            totalHospitalBeds: 1380614
+            };
+
+        const output = 
+        { 
+            data: input,
+            impact: {
+                currentlyInfected: 6740,
+                infectionsByRequestedTime: 6901760
+            },
+            severeImpact: {
+                currentlyInfected: 33700,
+                infectionsByRequestedTime:34508800            
+            }
+        }
+
+        expect(covid19ImpactEstimator(input)).toEqual(output)
+    })
+})
+
+//function filter(a){
+  //  return([{id:3, url: "https://www.rr1.f"}])
+//}
+
--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,26 @@
-const covid19ImpactEstimator = (data) => data;
+
+
+const covid19ImpactEstimator = (data) => {
+const  currentInfected = data.reportedCases * 10
+const expectedCases = currentInfected * 1024
+const severeInfected = data.reportedCases * 50
+const expectedSevereCases = severeInfected * 1024
+
+return {
+    data,
+    impact: {
+        currentlyInfected: currentInfected,
+        infectionsByRequestedTime: expectedCases
+     },
+    severeImpact: {
+        currentlyInfected: severeInfected,
+        infectionsByRequestedTime: expectedSevereCases
+    }
+    
+    }
+}
+ 
+
+
 
 export default covid19ImpactEstimator;
