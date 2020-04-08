28cc9a6f5255fd19e39b5a9a437d4809bffc806b
nerjib <kabirnajib0@gmail.com>
2020-04-08 23:04:31 +0000

    normalizing weeks and month in days

--- src/estimator.js
+++ src/estimator.js
@@ -7,9 +7,9 @@ const covid19ImpactEstimator = (data) => {
   switch (data.periodType) {
     case 'days':
       currentInfected = data.reportedCases * 10;
-      expectedCases = Math.round(currentInfected * (2 ** (data.timeToElapse / 3)));
+      expectedCases = (currentInfected * (2 ** Math.round(data.timeToElapse / 3)));
       severeInfected = data.reportedCases * 50;
-      expectedSevereCases = Math.round(severeInfected * (2 ** (data.timeToElapse / 3)));
+      expectedSevereCases = (severeInfected * (2 ** Math.round(data.timeToElapse / 3)));
       break;
     case 'weeks':
       currentInfected = data.reportedCases * 10;
