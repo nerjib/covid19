20fd3ff0971d5217df9a68784811ac6f9a20bb5b
nerjib <kabirnajib0@gmail.com>
2020-04-08 20:49:45 +0000

    fix linting issues

--- src/estimator.js
+++ src/estimator.js
@@ -1,26 +1,20 @@
-
-
+/* eslint-disable linebreak-style */
 const covid19ImpactEstimator = (data) => {
-const  currentInfected = data.reportedCases * 10
-const expectedCases = currentInfected * 1024
-const severeInfected = data.reportedCases * 50
-const expectedSevereCases = severeInfected * 1024
-
-return {
+  const currentInfected = data.reportedCases * 10;
+  const expectedCases = currentInfected * 1024;
+  const severeInfected = data.reportedCases * 50;
+  const expectedSevereCases = severeInfected * 1024;
+  return {
     data,
     impact: {
-        currentlyInfected: currentInfected,
-        infectionsByRequestedTime: expectedCases
-     },
+      currentlyInfected: currentInfected,
+      infectionsByRequestedTime: expectedCases
+    },
     severeImpact: {
-        currentlyInfected: severeInfected,
-        infectionsByRequestedTime: expectedSevereCases
+      currentlyInfected: severeInfected,
+      infectionsByRequestedTime: expectedSevereCases
     }
-    
-    }
-}
- 
-
-
+  };
+};
 
 export default covid19ImpactEstimator;
